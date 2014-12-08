7e9db4739794871cbb86cfcf96ffdb41aecacb41
Mark Paget <marcuspaget@gmail.com>
2014-12-08 16:37:13 +0800

    plotting first chart

--- plot1.R
+++ plot1.R
@@ -1,3 +1,3 @@
 q<-read.csv("household_power_consumption-subset.txt",sep=";")
-png(file = "plot1.png",height=480,width=480)
+png(file = "plot1.png",height=480,width=480, bg="transparent")
 hist(q$Global_active_power,freq=T,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
